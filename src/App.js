2d1ead6cbd7235f8910a32ca12260627bcbfa9cc
hoangganhh.do <hoangganhh.do@gmail.com>
2020-07-09 19:06:04 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input 
+              type="text" 
+              className="" 
+              placeholder="First Name" 
+              name="firstName" 
+              noValidate
+              onChange={this.handleChange}
+              />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input 
+              type="text" 
+              className="" 
+              placeholder="Last Name" 
+              name="lastName" 
+              noValidate
+              onChange={this.handleChange}
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input 
+              type="text" 
+              className="" 
+              placeholder="Email"
+              type="email" 
+              name="email" 
+              noValidate
+              onChange={this.handleChange}
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input 
+              type="text" 
+              className="" 
+              placeholder="Password" 
+              type="password"
+              name="password" 
+              noValidate
+              onChange={this.handleChange}
+              />
+            </div>
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+              <small>Already Have an Account?</small>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
